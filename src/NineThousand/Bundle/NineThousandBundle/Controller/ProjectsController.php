690b7310e236653b0f57b47b1f997cf7f48b6255
Jesse Greathouse <jesse.greathouse@gmail.com>
2011-07-11 23:47:19 -0500

    added project detail page, fixed styles

--- src/NineThousand/Bundle/NineThousandBundle/Controller/ProjectsController.php
+++ src/NineThousand/Bundle/NineThousandBundle/Controller/ProjectsController.php
@@ -3,6 +3,7 @@
 namespace NineThousand\Bundle\NineThousandBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 
 class ProjectsController extends Controller
@@ -13,6 +14,25 @@ class ProjectsController extends Controller
         return $this->render('NineThousandNineThousandBundle:Projects:index.html.twig', 
                 array(
                     'projects' => $this->container->getParameter('ninethousand.projects'),
+                ));
+    }
+    
+    public function projectAction($slug = null)
+    { 
+        $projects = $this->container->getParameter('ninethousand.projects');
+        $id = null;
+        foreach($projects as $key => $project) {
+            if ($project['slug'] == $slug) {
+                $id = $key;
+                break;
+            }
+        }
+        if (!$slug || !$id) {
+            throw new NotFoundHttpException('Project page was not found.');
+        }
+        return $this->render('NineThousandNineThousandBundle:Project:index.html.twig', 
+                array(
+                    'project' => $projects[$key],
                 ));
     }
 }
