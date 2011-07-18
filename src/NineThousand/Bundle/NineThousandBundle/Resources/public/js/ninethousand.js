b05acfcd7031592aa1d23a18437262b6bb8c82d4
Jesse Greathouse <jesse.greathouse@gmail.com>
2011-07-17 21:32:18 -0500

    fixed a few broken links and added a javascript to preload rollover images

--- src/NineThousand/Bundle/NineThousandBundle/Resources/public/js/ninethousand.js
+++ src/NineThousand/Bundle/NineThousandBundle/Resources/public/js/ninethousand.js
@@ -7,3 +7,22 @@ jQuery(document).ready(function(){
         }
     }); 
 });
+
+// array of images 
+var imgs = new Array(4); 
+    imgs[0] = "/bundles/ninethousandninethousand/images/blue-hover.png"; 
+    imgs[1] = "/bundles/ninethousandninethousand/images/green-hover.png"; 
+    imgs[2] = "/bundles/ninethousandninethousand/images/orange-hover.png";
+    imgs[3] = "/bundles/ninethousandninethousand/images/button.gif";  
+  
+// preload the images 
+function preload() { 
+    var tmp = null; 
+    for (var j = 0; j < imgs.length; j++) { 
+        tmp = imgs[j]; 
+        imgs[j] = new image(); 
+        imgs[j].src = tmp; 
+    } 
+} 
+
+void(preload()); 
